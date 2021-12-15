cc6ddf7ad39889f385da88a8fae3be341fa4eb9e
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-15 13:17:04 -0500

    Added voting functionality

---
+++ samples/samples.js
@@ -0,0 +1,21 @@
+// USER 
+{
+    "username": "Rae",
+    "email": "rae@eeeee.com",
+    "password": "example2"
+}
+
+// POST
+
+{
+    "title": "Cool title",
+    "post_url": "www.example.com",
+    "user_id": 1
+}
+
+// VOTE
+
+{
+    "user_id": 1,
+    "post_id": 1
+}
