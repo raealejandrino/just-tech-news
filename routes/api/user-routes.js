6c7ed0d3662eec44562825221b884b0bb7b3554d
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-18 14:24:48 -0500

    Initialize files for MVC

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -129,6 +129,7 @@ router.put('/:id', (req, res) => {
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
+    // Delete
     User.destroy({
         where: {
           id: req.params.id
